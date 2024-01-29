30081c038abe09cec9b2a1bec6db50c00e225ae8
Liliia Botueva <43854435+botuevaliliya@users.noreply.github.com>
2024-01-29 22:11:35 +0300

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -442,8 +442,27 @@ class Solution:
             s = add_letter(s, phone[i])
         return s
 
+# 141. Linked List Cycle
+# Given head, the head of a linked list, determine if the linked list has a cycle in it.
+# There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+# Return true if there is a cycle in the linked list. Otherwise, return false.
 
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return False
+        run2 = head
+        while head.next and run2.next and run2.next.next:
+            head = head.next
+            run2 = run2.next.next
+            if head==run2:
+                return True
+        return False
 
 
 
