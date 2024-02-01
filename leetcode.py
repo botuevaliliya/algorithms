714f9ff032899c3dcf2e5304fceee9fd0fc033ac
Liliia Botueva <43854435+botuevaliliya@users.noreply.github.com>
2024-02-01 23:43:43 +0300

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -694,5 +694,42 @@ class Solution:
             return
         quickSort(nums, n-1, l-1, 0)
 
+# 33. Search in Rotated Sorted Array
+# There is an integer array nums sorted in ascending order (with distinct values).
+# Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+# Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
 
+class Solution:
+    def search(self, nums, target: int) -> int:
+        l = 0 
+        r = len(nums)-1
+        if nums[r]==target:
+            return r
+        elif nums[l]==target:
+            return l
+        while l<r-1:
+            m = l+(r-l)//2
+            if nums[m]==target:
+                return m
+            elif nums[r]==target:
+                return r
+            elif nums[l]==target:
+                return l
+            elif l==m or m==r:
+                return -1
+            elif nums[m]>target and target>nums[l]:
+                r = m 
+            elif nums[m]<target and target<nums[r]:
+                l = m
+            elif target>nums[m] and target>nums[r] and nums[r]>nums[m]:
+                r = m
+            elif target>nums[m] and target>nums[r] and nums[r]<nums[m]:
+                l = m
+            elif target<nums[m] and nums[r]<nums[m]:
+                l = m
+            elif target<nums[m] and nums[r]>nums[m]:
+                r = m
+            else:
+                return -1
+        return -1
 
