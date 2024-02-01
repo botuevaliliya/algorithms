d17c6710f2935bb6ddb8ae7fa63335f9ffb6e616
Liliia Botueva <43854435+botuevaliliya@users.noreply.github.com>
2024-02-01 18:44:50 +0300

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -566,7 +566,40 @@ class Solution:
             end = end.next
         return True
 
+# 160. Intersection of Two Linked Lists
+# Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
 
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode):
+        headAc = headA
+        headBc = headB
+        headAcc = headA
+        headBcc = headB
+        while headAcc and headAcc.next:
+            headAcc = headAcc.next
+        while headBcc and headBcc.next:
+            headBcc = headBcc.next
+        if headBcc!=headAcc:
+            return
+        
+
+        while headA!=headB:
+            if headA:
+                headA = headA.next
+            else:
+                headA = headAc
+            if headB:
+                headB = headB.next
+            else:
+                headB = headBc
+        return headA
+        
+        
         
 
 
