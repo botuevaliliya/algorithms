f0ddd039a279ec282298b1420b090ba7eb398326
Liliia Botueva <43854435+botuevaliliya@users.noreply.github.com>
2024-02-02 23:50:00 +0300

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -922,7 +922,20 @@ class Solution:
         add_line(0, 0, 'r', s, n, m)
         return result
 
+# 55. Jump Game
+# You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+# Return true if you can reach the last index, or false otherwise.
 
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums) - 1
+        mi = n
+        for i in range(n,-1,-1):
+            if mi-i<=nums[i]:
+                    mi = i
+        if mi==0:
+            return True
+        return False
 
 
 
