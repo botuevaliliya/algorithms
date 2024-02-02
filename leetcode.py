03760a59dfca8233454f83102643225abd478646
Liliia Botueva <43854435+botuevaliliya@users.noreply.github.com>
2024-02-02 18:42:14 +0300

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -864,9 +864,20 @@ class Solution:
             n -= 1
             i += 1
 
+# 49. Group Anagrams
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
-
-
+class Solution:
+    def groupAnagrams(self, strs):
+        has = {}
+        for i in strs:
+            k = ''.join(sorted(i))
+            if k not in has.keys():
+                has[k] = [i]
+            else:
+                has[k].append(i)
+        return list(has.values())
 
 
 
