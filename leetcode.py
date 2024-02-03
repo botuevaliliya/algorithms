8b0b41d925676395dafb71eee29cd00d8f6abdcd
Liliia Botueva <43854435+botuevaliliya@users.noreply.github.com>
2024-02-03 15:09:16 +0300

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -954,7 +954,28 @@ class Solution:
                 
         return result
             
+# 62. Unique Paths
+# There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+# Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+# The test cases are generated so that the answer will be less than or equal to 2 * 109.
 
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # def all_solutions(m, n):
+        #     if m==1 or n==1:
+        #         return 1
+        #     return all_solutions(m-1, n) + all_solutions(m, n-1)
+
+        # return all_solutions(m, n)
+        result = [[]]
+        for i in range(m):
+            for j in range(n):
+                if i==0 or j==0:
+                    result[i].append(1)
+                else:
+                    result[i].append(result[i-1][j] + result[i][j-1])
+            result.append([])
+        return result[m-1][n-1]
 
 
 
