bf38f563a0a9a833a07fa61a5d2c8aeebc4e08e1
Liliia Botueva <43854435+botuevaliliya@users.noreply.github.com>
2024-02-07 21:58:54 +0300

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -1180,7 +1180,20 @@ class Solution:
             m = j
         return m
 
+# 142. Linked List Cycle II
+# Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+# There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+# Do not modify the linked list.
 
-
-
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return 
+        has = {}
+        while head not in has:
+            if head.next==None:
+                return 
+            has[head] = 1
+            head = head.next
+        return head
 
