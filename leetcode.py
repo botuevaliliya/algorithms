c8cdba69e920bac9760b30fd359dcf51773be54f
Liliia Botueva <43854435+botuevaliliya@users.noreply.github.com>
2024-02-11 18:47:08 +0300

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -1333,9 +1333,30 @@ class Solution:
         
         return result
 
+# 72. Edit Distance
+# Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+# You have the following three operations permitted on a word:
+# Insert a character
+# Delete a character
+# Replace a character
 
-
-
+class Solution:
+    def minDistance(self, word1, word2):
+        n, m = len(word1), len(word2)
+        if n > m:
+            word1, word2 = word2, word1
+            n, m = m, n
+        current_row = list(range(n + 1))
+        for i in range(1, m + 1):
+            previous_row, current_row = current_row, [i] + [0] * n
+            for j in range(1, n + 1):
+                add, delete, change = previous_row[j] + 1, current_row[j - 1] + 1, previous_row[j - 1]
+                if word1[j - 1] != word2[i - 1]:
+                    change += 1
+                current_row[j] = min(add, delete, change)
+        return current_row[n]
+            
+            
 
 
 
