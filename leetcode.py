fdeffca142094cc1fe071f885283c59431737059
Liliia Botueva <43854435+botuevaliliya@users.noreply.github.com>
2024-02-24 21:50:37 +0300

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -1663,4 +1663,44 @@ class Solution:
                 return min(nums[l], nums[r], nums[m])
         return min(nums[l], nums[r])
 
+# 155. Min Stack
+# Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+# Implement the MinStack class:
+# MinStack() initializes the stack object.
+# void push(int val) pushes the element val onto the stack.
+# void pop() removes the element on the top of the stack.
+# int top() gets the top element of the stack.
+# int getMin() retrieves the minimum element in the stack.
+# You must implement a solution with O(1) time complexity for each function.
 
+class MinStack:
+    def __init__(self):
+        self.list = []
+        self.minimum = None
+
+    def push(self, val: int) -> None:
+        if self.list == [] or val<self.minimum:
+            self.minimum = val
+        self.list.append(val)
+
+    def pop(self) -> None:
+        if self.minimum==self.list[-1]:
+            if self.list[:-1]==[]:
+                self.minimum = None
+            else:
+                self.minimum = min(self.list[:-1])
+        self.list.pop()
+
+    def top(self) -> int:
+        return self.list[-1]
+
+    def getMin(self) -> int:
+        return  self.minimum
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
