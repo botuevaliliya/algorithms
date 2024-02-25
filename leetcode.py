380ce13e1ea7069476e54c6882d16822d9881976
Liliia Botueva <43854435+botuevaliliya@users.noreply.github.com>
2024-02-25 21:48:43 +0300

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -1984,3 +1984,21 @@ class Solution:
             if nums[i]==nums[i-1]:
                 return nums[i]
 
+
+
+# 300. Longest Increasing Subsequence
+# Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        lis = [1]*n
+        for i in range(1, n):
+            for j in range(0, i):
+                if nums[i] > nums[j] and lis[i] < lis[j] + 1:
+                    lis[i] = lis[j]+1
+        return max(lis)
+
+
+
+	    
