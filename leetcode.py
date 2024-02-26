931aca49da16319fa41fdc4d3e2cfd35b321987f
Liliia Botueva <43854435+botuevaliliya@users.noreply.github.com>
2024-02-26 22:06:50 +0300

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -2158,3 +2158,36 @@ class Solution:
         return l or r
 
 
+# 240. Search a 2D Matrix II
+# Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+# Integers in each row are sorted in ascending from left to right.
+# Integers in each column are sorted in ascending from top to bottom.
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        n = len(matrix[0])
+
+        for row in range(m):
+            if target < matrix[row][0]:
+                return False
+            if target > matrix[row][n-1]:
+                continue
+            left = 0
+            right = n-1
+            while left <= right:
+                mid = (left+right)//2
+                if matrix[row][mid] == target:
+                    return True
+                if matrix[row][mid] > target:
+                    right = mid-1
+                else:
+                    left = mid+1
+
+
+
+
+
+
+			
