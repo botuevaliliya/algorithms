f8fb98cf9feeb5d118f610f88934c22486491425
Liliia Botueva <43854435+botuevaliliya@users.noreply.github.com>
2024-02-26 22:40:31 +0300

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -2186,8 +2186,25 @@ class Solution:
                     left = mid+1
 
 
+# 438. Find All Anagrams in a String
+# Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        def make_dict(word):
+            d = {i:0 for i in set(word)}
+            for i in word:
+                d[i] += 1
+            return d
 
-
+        k = len(p)
+        p = make_dict(p)
+        result = []
+        for i in range(len(s)-k+1):
+            if set(s[i:i+k])==set(p):
+                if make_dict(s[i:i+k])==p:
+                    result.append(i)
+        return result
 
 			
