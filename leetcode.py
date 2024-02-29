13b17b16a2e718ddf7ca8f74b4d3f5d74bb7c36a
Liliia Botueva <43854435+botuevaliliya@users.noreply.github.com>
2024-02-29 22:10:46 +0300

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -2322,3 +2322,26 @@ class Solution:
                     result[i] = k - i
         return result
 
+
+# 1143. Longest Common Subsequence
+# Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n, m = len(text1), len(text2)
+        result = [[0]*(m+1) for i in range(n+1)]
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                if text1[i-1] == text2[j-1]:
+                    result[i][j] = result[i-1][j-1] + 1
+                else:
+                    result[i][j] = max(result[i-1][j], result[i][j-1])
+        return result[n][m]
+
+
+
+
+
+
+
+
