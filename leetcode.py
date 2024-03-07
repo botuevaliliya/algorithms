2383abb1c76b6bc8acbe2b8763b1e0873569eb36
Liliia Botueva <43854435+botuevaliliya@users.noreply.github.com>
2024-03-07 23:00:11 +0300

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -2480,5 +2480,54 @@ class Solution:
         # print(max_k)
         return False
                 
-                
+
+
+# 23. Merge k Sorted Lists
+# You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+# Merge all the linked-lists into one sorted linked-list and return it.
                    
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists or lists==[]:
+            return 
+
+        def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+            head = ListNode()
+            curr = head
+            while list1 and list2:
+                if list1.val<=list2.val:
+                    curr.next = list1
+                    curr = list1 
+                    list1 = list1.next
+                else:
+                    curr.next = list2
+                    curr = list2
+                    list2 = list2.next
+            if list1 or list2:
+                curr.next = list1 if list1 else list2 
+            return head.next
+
+        new_list = []
+        while len(lists)!=1:
+            new_list = []
+            for i in range(1, len(lists), 2):
+                new_list.append(mergeTwoLists(lists[i], lists[i-1]))
+            if i!=len(lists)-1:
+                new_list.append(lists[-1])
+            lists = new_list
+        
+        return lists[0]
+            
+
+
+
+
+
+
+
+
