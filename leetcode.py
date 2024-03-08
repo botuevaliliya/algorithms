cb96d1657492b10e87597e831cf2a511a9dd0ae9
Liliia Botueva <43854435+botuevaliliya@users.noreply.github.com>
2024-03-08 19:48:19 +0300

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -2843,3 +2843,67 @@ class MedianFinder:
 # obj.addNum(num)
 # param_2 = obj.findMedian()
 	
+# 76. Minimum Window Substring
+# Given two strings s and t of lengths m and n respectively, return the minimum window 
+# substring of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+# The testcases will be generated such that the answer is unique.
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if t=='':
+            return t
+
+        def make_dict(word):
+            d = {i:0 for i in set(word)}
+            for i in word:
+                d[i] += 1
+            return d
+
+        # n = len(s)
+        # d = make_dict(t)
+        # i, k = 0, 0
+        # result = ''
+        # while i<n and s[i] not in d:
+        #     i += 1
+        # while i<n:
+        #     if s[i] in d:
+        #         d[s[i]] -= 1
+        #         if d[s[i]]<0:
+        #             while s[i-k]!=s[i]:
+        #                 if s[i-k] in d:
+        #                     d[s[i-k]] += 1
+        #                 k -= 1
+        #             d[s[i]] += 1
+        #             k -= 1
+        #             while s[i-k] not in d:
+        #                 k -= 1
+        #         if sum(d.values())==0:
+        #             # print('result', s[i-k:i+1])
+        #             if len(result)>k or result=='':
+        #                 result = s[i-k:i+1]
+        #     i += 1
+        #     k += 1
+        #     # print(i, d, k)
+        # return result
+
+        countT, window = {}, {}
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+        have, need = 0, len(countT)
+        res, resLen = [-1, -1], float('inf')
+        l = 0 
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+            if c in countT and window[c]==countT[c]:
+                have += 1
+            while have==need:
+                if (r-l+1)<resLen:
+                    res = [l, r]
+                    resLen = (r - l + 1)
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]]<countT[s[l]]:
+                    have -= 1
+                l += 1
+        l, r = res
+        return s[l:r+1] if resLen != float('inf') else ''
