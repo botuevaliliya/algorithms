9c651319e1e33df69a8a1c3bb6539d22ca0c8371
Liliia Botueva <43854435+botuevaliliya@users.noreply.github.com>
2024-03-08 18:54:55 +0300

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -2804,3 +2804,42 @@ class Solution:
                 l += 1
             r += 1
         return result 
+
+
+# 295. Find Median from Data Stream
+# The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value, and the median is the mean of the two middle values.
+# Implement the MedianFinder class:
+# MedianFinder() initializes the MedianFinder object.
+# void addNum(int num) adds the integer num from the data stream to the data structure.
+# double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+
+class MedianFinder:
+
+    def __init__(self):
+        self.minheap = []
+        self.maxheap = []
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.maxheap, -1 * num)
+        if self.maxheap and self.minheap and -1*self.maxheap[0]>self.minheap[0]:
+            val = -1 * heapq.heappop(self.maxheap)
+            heapq.heappush(self.minheap, val)
+        if len(self.maxheap)>len(self.minheap)+1:
+            val = -1 * heapq.heappop(self.maxheap)
+            heapq.heappush(self.minheap, val)
+        if len(self.minheap)>len(self.maxheap)+1:
+            val = heapq.heappop(self.minheap)
+            heapq.heappush(self.maxheap, -1 * val)
+
+    def findMedian(self) -> float:
+        if len(self.maxheap)>len(self.minheap):
+            return -self.maxheap[0]
+        if len(self.minheap)>len(self.maxheap):
+            return self.minheap[0]
+        return (-self.maxheap[0]+self.minheap[0])/2
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
+	
